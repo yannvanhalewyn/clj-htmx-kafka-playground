e4e9c62b3b428d18601ac78a2dafd7ebd03d2259
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2025-06-11 02:57:18 +0200

    (refactor): declare pipelines in Clojure

--- src/my_app/events.clj
+++ src/my_app/events.clj
@@ -4,148 +4,78 @@
     [integrant.core :as ig]
     [jackdaw.admin :as ja]
     [jackdaw.client :as jc]
-    [jackdaw.serdes.edn :as jedn]
     [malli.core :as m]
     [malli.error :as me]
-    [my-app.tools.date :as date])
+    [my-app.tools.date :as date]
+    [my-app.tools.etl-topology :as etl-topology])
   (:import
-    [java.time Duration Instant]
-    [org.apache.kafka.clients.consumer ConsumerRecord]
-    [org.apache.kafka.common.errors WakeupException]))
+    [org.apache.kafka.clients.consumer ConsumerRecord]))
 
 (defmethod print-method java.time.Instant
   [inst ^java.io.Writer w]
   (.write w (str "#instant \"" (.toString inst) "\"")))
 
-(defn- edn-serde []
-  (jedn/serde {:readers {'instant Instant/parse}}))
-
-(defmulti process-batch! :handler)
-
-(defmethod process-batch! :my-app.events/calculate-flight-durations
-  [{:keys [producer]} records]
+(defn calculate-flight-durations
+  [{:keys [connector/producers]} records]
   (let [processed-count (atom 0)
-        error-count (atom 0)]
+        error-count (atom 0)
+        producer (get producers :topic/flight-durations)]
     (doseq [^ConsumerRecord record records]
-      (log/debug "Producing flight duration record")
+      (log/debug "[Durations] Producing flight duration record")
       (try
         (jc/produce! (:producer producer) (:topic producer) (.key record)
           ;; TODO calculate duration, can't do it without joining stream...
           {:duration 10})
         (swap! processed-count inc)
         (catch Exception e
-          (log/error e "Failed to calculate duration:" (.key record))
+          (log/error e "[Durations] Failed to calculate duration:" (.key record))
           (swap! error-count inc))))
     {:processed @processed-count
      :errors @error-count
      :total (.count records)}))
 
-(defmethod process-batch! :my-app.events/warn-delayed-flights
+(defn warn-delayed-flights
   [_ records]
-  (log/debug "Processing delayed flight warnings: " (.count records))
+  (log/debug "[Delayed Flights] Processing delayed flight warnings: " (.count records))
   (doseq [^ConsumerRecord record records]
     (let [payload (.value record)]
       (case (:event-type payload)
         :flight-departed
         (when (> (inst-ms (:time payload))
                 (inst-ms (:scheduled-departure payload)))
-          (log/warnf "Delayed flight: [%s] departed at %s while scheduled at %s"
+          (log/warnf "[Delayed Flights] Delayed flight: [%s] departed at %s while scheduled at %s"
             (:flight payload)
             (date/format-date (:time payload))
             (date/format-date (:scheduled-departure payload))))
         :flight-arrived
         (when (> (inst-ms (:time payload))
                 (inst-ms (:scheduled-arrival payload)))
-          (log/warnf "Delayed flight: [%s] arrived at %s while scheduled at %s"
+          (log/warnf "[Delayed Flights] Delayed flight: [%s] arrived at %s while scheduled at %s"
             (:flight payload)
             (date/format-date (:time payload))
             (date/format-date (:scheduled-arrival payload))))
-        (log/debug "Different event type" payload)))))
+        (log/debug "[Delayed Flights] Different event type" payload)))))
 
-(defmethod ig/init-key :my-app.kafka/config
+(defmethod ig/init-key ::kafka-config
   [_ config]
   config)
 
-(defn- start-etl-consumer! [component {:keys [topics kafka-config]}]
-  (let [{:keys [group-id subscriptions poll-duration]} (:consumer component)
-        consumer (jc/subscribed-consumer
-                   (assoc (select-keys kafka-config ["bootstrap.servers"])
-                     "group.id" group-id)
-                   (map topics subscriptions))
-        running? (atom true)]
-    (log/info "Starting etl processor for " (:handler component))
-    (future
-      (try
-        (while @running?
-          (let [records (.poll consumer (Duration/ofMillis poll-duration))]
-            (when (pos? (.count records))
-              (log/info "Processing batch of" (.count records) "records")
-              (let [result (process-batch! component records)]
-                (log/info "Batch processed:" result)
-                (.commitSync consumer)
-                (log/debug "Committed offsets")))))
+(defmethod ig/init-key ::flights-pipeline
+  [_ config]
+  (-> (etl-topology/new config)
+    (etl-topology/add-source :source/flight-events :topic/flight-events)
+    (etl-topology/add-connector :connector/flight-durations
+      {:in [:topic/flight-events]
+       :out [:topic/flight-durations]}
+      calculate-flight-durations)
+    (etl-topology/add-sink :sink/duration-warnings
+      {:in [:topic/flight-events]}
+      warn-delayed-flights)
+    (etl-topology/start!)))
 
-        (catch WakeupException _e
-          (log/info "ETL processor woken up. Shutting down"))
-
-        (catch Exception e
-          (log/error e "ETL processor error. Shutting down"))
-
-        (finally
-          (log/info "ETL processor stopped. Shutting down")
-          (log/debug "Closing consumer")
-          (.close consumer))))
-
-    (assoc component
-      :consumer consumer
-      :running? running?)))
-
-(defn- assoc-producer [component kafka-config topics]
-  (let [topic (and (:producer component)
-                   (get topics (:producer component)))]
-    (cond-> component
-      topic
-      (assoc :producer
-        {:producer (jc/producer kafka-config topic)
-         :topic topic}))))
-
-(defn- topic-config [config]
-  (assoc config
-    :key-serde (edn-serde)
-    :value-serde (edn-serde)))
-
-(defmethod ig/init-key :my-app/etl-topology
-  [_ {:keys [kafka-config topics components]}]
-  (log/debug "INIT TOPOLOGY" kafka-config topics)
-  ;;(ja/create-topics! admin-client (vals topic))
-  (let [topics (update-vals topics topic-config)]
-    {:components
-     (doall
-       (for [component components]
-         (case (:type component)
-           :producer
-           ;; I think these need to be closed? So maybe need to be integrant
-           ;; components
-           (assoc-producer component kafka-config topics)
-           :consumer
-           (-> component
-             (assoc-producer kafka-config topics)
-             (start-etl-consumer!
-               {:kafka-config kafka-config
-                :topics topics})))))}))
-
-(defmethod ig/halt-key! :my-app/etl-topology
+(defmethod ig/halt-key! ::flights-pipeling
   [_ topology]
-  (doseq [component (:components topology)]
-    (when (:producer component)
-      (log/debug "Closing producer" (get-in component [:producer :topic :topic-name])))
-    (some-> (:producer component) :producer (.close))
-    (when (:consumer component)
-      (log/debug "Waking consumer"))
-    (some-> (:consumer component) (.wakeup))
-    (when (:running? component)
-      (log/debug "Setting running? to false"))
-    (some-> (:running? component) (reset! false))))
+  (etl-topology/stop! topology))
 
 (def FlightEvent
   [:map
@@ -153,11 +83,11 @@
    [:event-type [:enum :flight-departed :flight-arrived :passenger-boarded]]
    [:time inst?]
    ;; For passenger events
-   [:who {:optional? true} :string]
+   [:who {:optional true} :string]
    ;; For departure events
-   [:scheduled-departure {:optional? true} inst?]
+   [:scheduled-departure {:optional true} inst?]
    ;; For departure events
-   [:scheduled-arrival {:optional? true} inst?]])
+   [:scheduled-arrival {:optional true} inst?]])
 
 (defn validate-flight-event!
   [payload]
@@ -167,19 +97,11 @@
               :payload payload})))
   payload)
 
-(defn ingest-data!
-  "Helper to ingest raw data into the archive topic"
-  [producer topic key data]
-  (validate-flight-event! data)
-  (jc/produce! producer topic key data)
-  (log/info "Ingested data:" key)
-  :ok)
-
 (comment
   (require '[my-app.tools.jackdaw :as tools.jackdaw])
   ;; Setup
   (def config (my-app.config/system-config {:profile :dev}))
-  (def admin-client (ja/->AdminClient (:my-app.kafka/config config)))
+  (def admin-client (ja/->AdminClient (::kafka-config config)))
 
   (ja/list-topics admin-client)
   (ja/delete-topics! admin-client
@@ -187,29 +109,34 @@
   (tools.jackdaw/re-delete-topics (:my-app.kafka/config config)
     #"^dev-etl.*")
 
-  (def producer
-    (-> (:my-app/etl-topology integrant.repl.state/system)
-      (:components)
-      (first)
-      :producer))
+  (def topology
+    (::flights-pipeline integrant.repl.state/system))
 
-  (def producer
-    (let [topic (topic-config {:topic-name "dev-etl-flight-events"})]
-      {:topic topic
-       :producer
-       (jc/producer {"bootstrap.servers" "localhost:29092"}
-         (topic-config {:topic-name "dev-etl-flight-events"}))}))
+  (dotimes [n 1000]
+   (etl-topology/send! topology :source/flight-events
+     {:flight "UA102"}
+     {:flight "UA102"
+      :event-type :flight-departed
+      :time #inst "2025-06-10T12:25:40.000-00:00"
+      :scheduled-departure #inst "2025-06-10T09:25:40.000-00:00"}))
 
-  (jc/produce! (:producer producer) (:topic producer)
-    {:flight "UA102"}
-    {:flight "UA102"
-     :event-type :flight-departed
-     :time #inst "2025-06-10T12:25:40.000-00:00"
-     :scheduled-departure #inst "2025-06-10T09:25:40.000-00:00"})
-
-  (jc/produce! (:producer producer) (:topic producer)
+  (etl-topology/send! topology :source/flight-events
     {:flight "UA102"}
     {:flight "UA102"
      :event-type :flight-arrived
      :time #inst "2025-06-10T12:25:40.000-00:00"
-     :scheduled-arrival #inst "2025-06-10T11:25:40.000-00:00"}))
+     :scheduled-arrival #inst "2025-06-10T11:25:40.000-00:00"})
+
+  (def producer
+    (let [topic (#'etl-topology/topic-config {:topic-name "dev-etl-flight-events"})]
+      {:topic topic
+       :producer
+       (jc/producer {"bootstrap.servers" "localhost:29092"} topic)}))
+
+  (dotimes [n 100]
+    (jc/produce! (:producer producer) (:topic producer)
+      {:flight "UA102"}
+      {:flight "UA102"
+       :event-type :flight-departed
+       :time #inst "2025-06-10T12:25:40.000-00:00"
+       :scheduled-departure #inst "2025-06-10T09:25:40.000-00:00"})))
---
+++ src/my_app/tools/etl_topology.clj
@@ -0,0 +1,170 @@
+(ns my-app.tools.etl-topology
+  (:require
+   [clojure.tools.logging :as log]
+   [jackdaw.client :as jc]
+   [jackdaw.serdes.edn :as jedn]
+   [malli.core :as m])
+  (:import
+   [java.time Duration Instant]
+   [org.apache.kafka.clients.consumer KafkaConsumer]
+   [org.apache.kafka.clients.producer KafkaProducer]
+   [org.apache.kafka.common.errors WakeupException]))
+
+(defn- kafka-producer? [x]
+  (instance? KafkaProducer x))
+
+(defn- kafka-consumer? [x]
+  (instance? KafkaConsumer x))
+
+(def TopicConfig
+  (m/schema
+    [:map
+     [:topic-name :string]
+     [:partition-count {:optional true} :int]
+     [:replication-factor {:optional true} :int]
+     [:topic-config {:optional true} :map]]))
+
+(def Source
+  (m/schema
+    [:map
+     [:source/producer [:fn kafka-producer?]]
+     [:source/topic TopicConfig]]))
+
+(def Connector
+  (m/schema
+    [:map
+     [:connector/consumer [:fn kafka-consumer?]]
+     [:connector/producers [:map-of :keyword
+                            [:map
+                             [:topic TopicConfig]
+                             [:producer [:fn kafka-producer?]]]]]
+     [:connector/handler fn?]]))
+
+(def Sink
+  [:map
+   [:sink/consumer [:fn kafka-consumer?]]])
+
+(def Topology
+  (m/schema
+    [:map
+     [::topics [:map-of :keyword TopicConfig]]
+     [::kafka-config [:map-of :string :string]]
+     [::sources [:map-of :keyword Source]]
+     [::connectors [:map-of :keyword Connector]]
+     [::sinks [:map-of :keyword Sink]]]))
+
+(defn- edn-serde []
+  (jedn/serde {:readers {'instant Instant/parse}}))
+
+(defn- topic-config [config]
+  (assoc config
+    :key-serde (edn-serde)
+    :value-serde (edn-serde)))
+
+(defn new [{:keys [kafka-config topics consumer-configs]}]
+  {::topics (update-vals topics topic-config)
+   ::kafka-config kafka-config
+   ::consumer-configs consumer-configs})
+
+(defn- consumer-config [{::keys [kafka-config consumer-configs]} component-key]
+  (assoc (select-keys kafka-config ["bootstrap.servers"])
+    "group.id" (get-in consumer-configs [component-key :group-id])
+    "client.id" (str "consumer-" (subs (str component-key) 1))))
+
+(defn- producer-config [{::keys [kafka-config]} component-key]
+  (merge kafka-config
+    {"acks" "all"
+     "client.id" (str "producer-" (subs (str component-key) 1))}))
+
+(defn add-source [topology source-key topic-key]
+  (let [topic (get-in topology [::topics topic-key])]
+    (assoc-in topology [::sources source-key]
+      {:source/producer (jc/producer (producer-config topology source-key) topic)
+       :source/topic topic})))
+
+(defn add-connector [topology connector-key {:keys [in out]} handler]
+  (assoc-in topology [::connectors connector-key]
+    {:connector/consumer (jc/subscribed-consumer
+                           (consumer-config topology connector-key)
+                           (map (::topics topology) in))
+     :connector/producers
+     (into {}
+       (for [topic-key out
+             :let [topic (get-in topology [::topics topic-key])]]
+         [topic-key
+          {:topic topic
+           :producer (jc/producer (producer-config topology topic-key) topic)}]))
+     :connector/handler handler}))
+
+(defn add-sink [topology sink-key {:keys [in]} handler]
+  (assoc-in topology [::sinks sink-key]
+    {:sink/consumer (jc/subscribed-consumer
+                      (consumer-config topology sink-key)
+                      (map (::topics topology) in))
+     :sink/handler handler}))
+
+(defn- start-polling! [component consumer handler]
+  (let [running? (atom true)
+        poll-duration 500]
+    (future
+      (try
+        (while @running?
+          (let [records (.poll consumer (Duration/ofMillis poll-duration))]
+            (when (pos? (.count records))
+              (log/info "Processing batch of" (.count records) "records")
+              (let [result (handler component records)]
+                (log/info "Batch processed:" result)
+                (.commitSync consumer)))))
+
+        (catch WakeupException _e
+          (log/info "ETL processor woken up. Shutting down"))
+
+        (catch Exception e
+          (log/error e "ETL processor error. Shutting down"))
+
+        (finally
+          (log/info "ETL processor stopped. Shutting down")
+          (.close consumer))))
+
+    (assoc component ::running? running?)))
+
+(defn start! [topology]
+  (sc.api/spy :start)
+  ;;(ja/create-topics! admin-client (vals topic))
+  (-> topology
+    (update ::connectors update-vals
+      (fn [{:connector/keys [consumer handler] :as connector}]
+        (start-polling! connector consumer handler)))
+    (update ::sinks update-vals
+      (fn [{:sink/keys [consumer handler] :as sink}]
+        (start-polling! sink consumer handler)))))
+
+(defn stop! [topology]
+  (sc.api/spy :stop!)
+  (doseq [source (vals (::sources topology))]
+    (log/debug "Closing producer" (:source/producer source)
+      (get-in source [:source/topic :topic-name]))
+    (.close (:source/producer source)))
+
+  (doseq [{::keys [running?] :as connector} (vals (::connectors topology))]
+    (log/debug "Waking consumer" (:connector/consumer connector))
+    (.wakeup (:connector/consumer connector))
+    (doseq [producer (vals (:connector/producers connector))]
+      (log/debug "Closing producer" (:producer producer) (get-in producer [:topic :topic-name]))
+      (.close (:producer producer)))
+    (reset! running? false))
+
+  (doseq [{::keys [running?] :as sink} (vals (::sinks topology))]
+    (log/debug "Waking consumer" (:sink/consumer sink))
+    (.wakeup (:sink/consumer sink))
+    (reset! running? false)))
+
+(defn send!
+  "Sends a record to a source consumer of the topology"
+  [topology source-key key data]
+  (let [source (get-in topology [::sources source-key])
+        producer (:source/producer source)
+        topic (:source/topic source)]
+    (jc/produce! producer topic key data))
+  (log/info "Ingested data:" key)
+  :ok)
